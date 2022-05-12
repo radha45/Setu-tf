16de0036326fc47210d325d714bf0fe13e535f44
radha45 <42378423+radha45@users.noreply.github.com>
2022-05-12 12:03:51 +0530

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -59,24 +59,6 @@ resource "aws_lb_target_group" "target_group" {
   }
 }
 
-# resource "aws_lb_listener" "listener" {
-#   load_balancer_arn = aws_lb.elb.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = var.ssl_certification_arn
-
-#   default_action {
-#     type = "fixed-response"
-
-#     fixed_response {
-#       content_type = "text/plain"
-#       message_body = "Invalid Path"
-#       status_code  = "404"
-#     }
-#   }
-# }
-
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.elb.arn
   port              = "80"
